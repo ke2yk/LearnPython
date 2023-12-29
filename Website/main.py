a7941ee1f8a19fe1d4741a8025552fdd85192b0e
Gary Utz <ke2yk48@gmail.com>
2023-12-29 17:12:47 -0500

    learning request url redirect get and post

--- Website/main.py
+++ Website/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 #Flask is cool
 
@@ -11,12 +11,26 @@ pages = [
     {'title': 'Contact', 'link': '/contact'},
     {'title': 'Photo Gallery', 'link': '/gallery'},
     {'title': 'History', 'link': '/history'},
-    {'title': 'About', 'link': '/about'}
+    {'title': 'About', 'link': '/about'},
+    {'title': 'Login', 'link': '/login'}
 ]
 
 # Routes for different pages
 @app.route('/')
 def home():
+    return render_template('index.html', pages=pages)  
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    print (f"<h1>{usr}</h1>")
     return render_template('index.html', pages=pages)
 
 @app.route('/calendar')
