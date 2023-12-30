061f62e897df70c396116bcb1dc85f629e69c41c
Gary Utz <ke2yk48@gmail.com>
2023-12-30 14:33:10 -0500

    First Commit For Forms Code

---
+++ forms/main.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, url_for, redirect
+
+#Flask is cool
+
+app = Flask(__name__)
+
+subscribers = []
+
+# Routes for different pages
+@app.route('/')
+def home():
+    title = "Club Home Page"
+    return render_template('index.html',title=title)  
+
+@app.route('/subscribe')
+def suscribe():
+    title = "subscribe to my newsletter"
+    return render_template("subscribe.html", title=title) 
+
+@app.route('/form', methods=["POST"])
+def form():
+    title = "Thank You!"
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    subscribers.append(f"{first_name} {last_name} | {email}")
+    return render_template("form.html", title=title, subscribers=subscribers) 
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    print (f"<h1>{usr}</h1>")
+    return render_template('index.html')
+
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
