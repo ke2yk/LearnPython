cda735f9fc8d5e4acb87cdb7025c2e390a1c9d26
Gary Utz <ke2yk48@gmail.com>
2024-01-01 10:20:27 -0500

    first push of new advanced forms code

---
+++ TestBed/lesson1_app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+# ...
+
+app = Flask(__name__)
+# ...
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd70d72ee73d021d8d8538c02620ca860c038687c0675a038'
+
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+
+@app.route('/')
+def index():
+    return render_template('index.html', messages=messages)# ...
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
